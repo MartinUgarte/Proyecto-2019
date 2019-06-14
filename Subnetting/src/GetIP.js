0f6871f31169aae8c0d934918856b20a3c63f36c
Dolni <39602138+Dolni@users.noreply.github.com>
2019-06-14 09:34:13 -0300

    axel preiti

--- Subnetting/src/GetIP.js
+++ Subnetting/src/GetIP.js
@@ -22,17 +22,83 @@ export default class GetIP extends Component {
     render() {
         return(
           <View style={styles.container}>
-            <Text></Text>
+          <StatusBar
+          barStyle="light-content"
+          />
+              <TextInput
+              placeholder="Username or Email" 
+              placeholderTextColor="rgba(255,255,255,0.7)"
+              returnKeyType="next"
+              onSubmitEditing={() => this.passwordInput.focus()}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styles.input} 
+              />
+              <TextInput
+              placeholder="Password" 
+              placeholderTextColor="rgba(255,255,255,0.7)"
+              returnKeyType="go"
+              secureTextEntry
+              style={styles.input}
+              ref={(input) => this.passwordInput} 
+              />
+
+              <Botoncito status={this.state.status} />
+              
+              <TouchableOpacity style={styles.buttonContainer1}>
+                  <Text                 
+                  style={styles.buttonText} onPress={this.clicked.bind(this)}>SORPRESA</Text>
+              </TouchableOpacity>
+
+              <Button
+              onPress={onPressSaludar}
+              title="Saludar"
+              color="#841584"
+              accessibilityLabel="Botón para saludar"
+              />
+
+              <ActivityIndicator size="large" color="#0000ff" />
+
+              <CheckBox
+              title='Click Here'
+              checked={this.state.checked}
+/>
+              
           </View>
         );
     }
 }
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+const onPressSaludar = () => {
+  alert('¡Hola, soy una alerta!')
+ }
+ 
+
+const styles = StyleSheet.create({
+  container: {
+      padding: 20,
+  },
+  input: {
+      height: 40,
+      backgroundColor: 'rgba(255,255,255,0.2)',
+      marginBottom: 20,
+      color: '#FFF',
+      paddingHorizontal: 10,
+  },
+  buttonContainer1: {
+      backgroundColor: '#2980b9',
+      paddingVertical: 15,
+      marginBottom: 20
+  },
+  buttonContainer2: {
+      backgroundColor: '#f92672',
+      paddingVertical: 15,
+      marginBottom: 20
+  },
+  buttonText: {
+      textAlign: 'center',
+      color: '#FFF',
+      fontWeight: '700'
+  }
+});
