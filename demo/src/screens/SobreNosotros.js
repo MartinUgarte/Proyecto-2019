024927184f7f878655337c9816128d4c1eccbda3
Dolni <39602138+Dolni@users.noreply.github.com>
2019-08-16 11:42:26 -0300

    Sobre Nostros js update

--- demo/src/screens/SobreNosotros.js
+++ demo/src/screens/SobreNosotros.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Image, Text, TouchableOpacity, ImageBackground } from 'react-native'
+import { StyleSheet, View, Image, Text, TouchableOpacity, ImageBackground, ScrollView } from 'react-native'
 
 export default class Bienvenida extends Component{
 
@@ -15,34 +15,20 @@ export default class Bienvenida extends Component{
 
                 <ImageBackground style={styles.container} source={require('../images/Estudio.jpg')} imageStyle={{opacity: 0.6}}>
                   
-                    <View style={styles.titleContainer}>
-                        <Text style={styles.titleText}>¡Conoce un poco más sobre nosotros!</Text>
+                    <View style={styles.header}>
+                            <MenuButton navigation={this.props.navigation} />
+                            <Text style={styles.titulo}>Sobre Nosotros</Text>
                     </View>
-                    
-                    <View style={styles.infoContainer}> 
-                            <View style={styles.textContainer}>
-                                <Image style={styles.imageStyle} source={require('../images/SobreNosotros_1.jpg')} />
-                            </View>
-                            <View style={styles.textContainer}>
-                                <Text style={styles.infoText}>Con MX, a través de una aplicación móvil, se logra mover un brazo robótico.</Text>
-                            </View>
-                    </View>
+                    <MenuButton navigation={this.props.navigation} />
 
-                    <View style={styles.infoContainer}> 
-                            <View style={styles.textContainer}>
-                                <Text style={styles.infoText}>Se conseguiría la exactitud requerida y un ahorro de tiempo significativo.</Text>
-                            </View>
-                            <View style={styles.textContainer}>
-                                <Image style={styles.imageStyle} source={require('../images/SobreNosotros_2.jpg')} />
-                            </View>
-                    </View>
-
-                    <View style={styles.infoContainer}> 
-                            <Image style={styles.imageStyle} source={require('../images/SobreNosotros_3.jpg')} />
-                            <View style={styles.textContainer}>
-                                <Text style={styles.infoText}>MX brinda presición, velocidad y comodidad.</Text>
-                            </View>
-                    </View>
+                    <ScrollView
+                        scrollEventThrottle={16}                       
+                    >
+                        <View style={styles.imagesContainer}>
+                            <Text>What can we help you find, Varun, I'm very funny xddd lol ajdiasjdi</Text>
+                        </View>
+                        
+                    </ScrollView>
 
                 </ImageBackground>
 
@@ -57,39 +43,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#000'
 
     },
-    
-    titleContainer: {
-        flex: 1,
-        justifyContent: 'center',
+    header: {
+        flex: 0.46,
+        backgroundColor: 'rgba(209,75,166,0.83)',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        marginHorizontal: 3
-    },
-    titleText: {
-        color: '#fff',
-        fontSize: 25,
-       // fontFamily: 'sans-serif',
-        textAlign: 'center'
         
     },
-    infoContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        marginHorizontal: 1
+    titulo: {
+        fontSize: 20,
+        marginLeft: 67,
+        marginTop: 10
+        
     },
-    imageStyle: {
-        width: 130,
-        height: 120,
-    },
-    textContainer: {
-        flex: 1,
-        marginHorizontal: 10,
-        marginVertical: 5,
-    },
-    infoText: {
-        color: '#fff',
-        fontSize: 15,
-        textAlign: 'center',
+    imagesContainer: {
+        flex: 1
     }
+
 });
