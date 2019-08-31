86b78a52fdb3f78544102a1b7c215e75112a66e5
preiti tasat axel facundo <43871824@comunidadort.edu.ar>
2019-08-31 17:49:05 -0300

    Implementación de Registro e Inicio de Sesión al servidor

--- server/app.js
+++ server/app.js
@@ -1,26 +1,39 @@
-//Crea Servidor Express.js
+//-Configuracion--------------------------------------------------------------------------------------------------------
+// Crea Servidor Express.js
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-//Indica el puerto por el que va a estar escuchando el Servidor
+// Indica el puerto por el que va a estar escuchando el Servidor
 var port = process.env.PORT || 3000
 app.listen(port, function () {
 	console.log('Corriendo en el puerto 3000')
 });
 
-//Configura el BodyParse
+// Configura el BodyParse
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
 app.use(bodyParser.json())
 
+// Configura Firebase
+var admin = require("firebase-admin");
+var serviceAccount = require("./MycroTechAdminSDK.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mycrotech-2019.firebaseio.com"
+});
+// Configura Cloud Firestore de Firebase
+var db = admin.firestore();
 
-//Variable que se va utilizar para responder al cliente
+//Configura Nodemailer (para enviar emails)
+var nodemailer = require("nodemailer");
+
+// Variable que se va utilizar para responder al cliente
 var respuesta;
 
-
-//Demuestra al celular donde se encuentra el servidor (IP)
+//-Simulación de brazo--------------------------------------------------------------------------------------------------------
+// Demuestra al celular donde se encuentra el servidor (IP)
 app.post('/server', function (req, res){
     respuesta = {
         msg: ""
@@ -52,3 +65,117 @@ app.post('/move', function (req, res){
     console.log("Listo");
     res.end(JSON.stringify(respuesta));
 })
+
+//-Cliente - Servidor - Base de Datos-----------------------------------------------------------------------------------
+// Registro
+app.post('/register', function (req, res) {
+	let repetido = false;
+	// Recibe la info
+	let nombreR = req.body.nombre;
+	let emailR = req.body.email;
+	let contraR = req.body.contra;
+	// Crea un documento en la collección Usuarios
+	var usuario = db.collection("Usuarios").doc(nombreR);
+	usuario.get()
+		// Verifica si el documento ya existe; si es así, envía error
+		.then(doc => {
+			if (doc.exists) {
+				reply = {
+					msg: 'Error'
+				};
+				res.end(JSON.stringify(reply));
+			} else {
+				// Verifica si el email ya fue usado
+				db.collection("Users").where('Email', '==', emailR).get()
+					.then(snapshot => {
+						snapshot.forEach(doc => {
+							repetido = true;
+							reply = {
+								msg: 'Error, mail'
+							};
+							res.end(JSON.stringify(reply));
+						});
+						// Si el email nunca fue usado, se crea un nuevo usuario en la Base de Datos
+						if (repetido === false) {
+							usuario.set({
+								Nombre_de_Usuario: nombreR,
+								Email: emailR,
+								Contraseña: contraR,
+								Verificado: false
+                            })
+                                // Luego, envia un mail al usuario
+								.then(function (docRef) {
+									var transporter = nodemailer.createTransport(({
+										service: 'gmail',
+										auth: {
+											type: 'OAuth2',
+											user: 'mycrotech2019@gmail.com',
+											password: 'MycroTechProyecto',
+											clientId: '610628515023-rmhl07nacbrguu44v6tbcmksoatdi4lf.apps.googleusercontent.com',
+											clientSecret: 'NJRUfYXTNoRXZEoDDYm9kf0R',
+											refreshToken: '1/s6tlQw3Pui36G9Ioyg-uOc5kGHKOisjMtADK4HoVq_ErwSRFC1TaVSU2OmqfyLx2',
+											accessToken: 'ya29.Glt1Bz0T3kJYIWPMrjwE2NZ4mRiMt_qozuVQHa828etEQCZVaV1ZllnescavrIVb9P5W_-HnkKs_a_ZlGqN2gC5RYVBYpOmPshG9ydRrK5kyubVwUgITIb0RyieE'
+										}
+									}));
+
+									var mailOptions = {
+										from: 'El equipo MycroTech <mycrotech2019@gmail.com>',
+										to: emailR,
+										subject: 'Registro Exitoso',
+										text: 'Buenos Dias, \r\n gracias por registrarse a MycroTech. A partir de ahora usted podrá tener la mejor experiencia con nuestros brazos robóticos. \r\n El equipo de MycroTech.'
+									}
+
+									transporter.sendMail(mailOptions, function (err, res) {
+										if (err) {
+											console.log('Error: ' + err);
+										} else {
+											console.log('Email sent');
+										}
+                                    })
+                                    
+									reply = {
+										msg: 'Registro existoso'
+									};
+									res.end(JSON.stringify(reply));
+                                })
+                                
+								.catch(function (error) {
+									console.error("Error adding document: ", error);
+								})
+						}
+					})
+			}
+		})
+});
+
+
+// Inicio de Sesión
+app.post('/login', function (req, res) {
+	// Recibe la info
+	let nombreL = req.body.usuario;
+	let contraL = req.body.contra;
+	var usuario = db.collection("Users").doc(nombreL);
+	usuario.get()
+		.then(doc => {
+			// Verifica si el usuario existe
+			if (doc.exists) {
+				// Verifica si la contraseña es correcta
+				if (doc.data().Contraseña === contraL) {
+					reply = {
+						msg: 'Usuario Encontrado'
+					};
+					res.end(JSON.stringify(reply));
+				} else {
+					reply = {
+						msg: 'Error, contra'
+					};
+					res.end(JSON.stringify(reply));
+				}
+			} else {
+				reply = {
+					msg: 'Error, usuario'
+				};
+				res.end(JSON.stringify(reply));
+			}
+		})
+});
